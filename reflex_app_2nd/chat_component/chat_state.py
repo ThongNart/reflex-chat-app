04e696e2896382238ddd9e079bbba33b840a0130
ThongNart <thongqtran@outlook.com>
2025-02-04 21:00:09 +0700

    add state logic and styling to the chat component

--- reflex_app_2nd/chat_component/chat_state.py
+++ reflex_app_2nd/chat_component/chat_state.py
@@ -1,16 +1,42 @@
 import reflex as rx
+import asyncio
+from typing import List
+
+
+class ChatMessage(rx.Base):
+    message: str
+    is_bot: bool = False
 
 
 class ChatState(rx.State):
 
     did_submit: bool = False
+    messages: List[ChatMessage] =[]
 
     @rx.var(cache=True)
     def user_did_submit(self) -> bool:
         return self.did_submit
-
-
-    def handle_submit(self, form_data:dict):
-        print('form data is here: ', form_data)
-        self.did_submit = True
     
+    def append_message(self, message, is_bot:bool=False):
+        self.messages.append(
+                ChatMessage(
+                    message = message,
+                    is_bot = is_bot
+                )
+            )
+
+
+
+    async def handle_submit(self, form_data:dict):
+        print('form data is here: ', form_data)
+        user_message = form_data.get('message')
+        if user_message:
+
+            self.did_submit = True
+            self.append_message(user_message, is_bot=False)
+
+            yield
+            await asyncio.sleep(2) #this can be used for api requests
+            self.did_submit = False
+            self.append_message(user_message, is_bot=True)
+            yield
